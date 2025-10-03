77d4223386bbec6654070222a4a92a49b29f6830
Abishek Kenneth <107182926+abi1035@users.noreply.github.com>
2025-10-03 11:31:03 -0400

    Added chili and Soup

--- src/CafeBilling.jsx
+++ src/CafeBilling.jsx
@@ -183,6 +183,23 @@ const items = [
     branches: ['HenleyPlace','BurtonManor'],
     totalPrice:3.75
   },
+   {
+    id: 25,
+    name: 'Chili',
+    image:'/Soup.png',
+    price:2.6548,
+    branches: ['HenleyPlace'],
+    totalPrice:3.00
+  },
+ 
+   {
+    id: 26,
+    name: 'Soup',
+    image:'/chili.png',
+    price:4.425,
+    branches: ['HenleyPlace'],
+    totalPrice:5.00
+  },
  
 ];
 
